dac9e90ed8ba821af545647938825eab65937c5c
wings <eternity.mr8@gmail.com>
2020-09-21 12:19:23 +0330

    Validate Shorten Validate shorten to be alphanumerical character.

--- controller/linkcontroller/linkcontroller.go
+++ controller/linkcontroller/linkcontroller.go
@@ -1,6 +1,7 @@
 package linkcontroller
 
 import (
+	"github.com/asaskevich/govalidator"
 	"github.com/eternity-wing/short_link/lib/baseconversion/base62"
 	"github.com/eternity-wing/short_link/repository/linkrepository"
 	"github.com/eternity-wing/short_link/service/linkservice"
@@ -9,9 +10,20 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+func New(c *fiber.Ctx) error {
+	return linkservice.ProcessNewLinkRequest(c)
+}
+
 func Get(c *fiber.Ctx) error {
 
 	shorten := c.Params("shorten")
+	if govalidator.IsAlphanumeric(shorten) != true {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"type":  "Validation error",
+			"title": "There was a validation error",
+			"error": "Url contains unallowable character(accepted characters are alphanumerical)",
+		})
+	}
 
 	convertor := shortlinkconversion.InitConvertor(base62.Convertor{})
 
@@ -22,8 +34,4 @@ func Get(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "The requested resource does not exist"})
 	}
 	return c.Redirect(link.URL, fiber.StatusMovedPermanently)
-}
-
-func New(c *fiber.Ctx) error {
-	return linkservice.ProcessNewLinkRequest(c)
 }
